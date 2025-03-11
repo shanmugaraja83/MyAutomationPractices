de3b345f6e057d082c0ec273be5c2fd87ac61f88
shanmugaraja83@yahoo.com <shanmugaraja83@yahoo.com>
2025-03-12 00:00:50 +0530

    committing the code for first program

---
+++ src/main/java/ProjectShan/ProjectShan/App.java
@@ -0,0 +1,10 @@
+package ProjectShan.ProjectShan;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/COM/AutomationPractice/FirstProgram.java
@@ -0,0 +1,27 @@
+package COM.AutomationPractice;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//String[] MyName = new String[3];
+		//MyName[0]="Raja";
+		//MyName[1]="Somu";
+		//MyName[2]="Gomu";
+		
+		//System.out.println(MyName[0]+" "+MyName[1]+" "+MyName[2]);
+		
+		String[] Pic= {"Murali","Ravi","Rajam" };
+		
+		System.out.println(Pic[0]+" "+Pic[1]+" "+Pic[2]);
+		
+		for (int i=0; i<Pic.length; i++)
+		{
+			System.out.println(Pic[i]);
+			
+		}
+		
+	}
+
+}
