d6e4f2d535fdec268538b0d49b5edf20bfa121b5
shanmugaraja83@yahoo.com <shanmugaraja83@yahoo.com>
2025-03-12 23:50:31 +0530

    new

---
+++ src/test/java/COM/AutomationPractice/AbstractImportedClass.java
@@ -0,0 +1,22 @@
+package COM.AutomationPractice;
+
+public class AbstractImportedClass extends AbstractProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AbstractImportedClass AIC= new AbstractImportedClass();
+AIC.Dog();
+AIC.man();
+	
+	}
+
+	@Override
+	public void Dog() {
+		// TODO Auto-generated method stub
+		System.out.println("Barking");
+	}
+
+	
+	
+	
+}
---
+++ src/test/java/COM/AutomationPractice/AbstractProgram.java
@@ -0,0 +1,15 @@
+package COM.AutomationPractice;
+
+public abstract class AbstractProgram {
+
+	
+public void man()
+{
+ String Sh="Talk";
+System.out.println(Sh + "This is from AbstaractProgram");
+}
+
+public abstract void Dog();
+
+	
+}
